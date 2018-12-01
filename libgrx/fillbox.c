1a6e6dcb69975f242ffe7f89744ce2300a77c111
Dan Morriss <daniel.morriss@visionstream.com.au>
2018-12-01 20:58:54 +1100

    wip grx.

--- libgrx/fillbox.c
+++ libgrx/fillbox.c
@@ -1,12 +1,22 @@
 #include "grx.h"
 
-int grx_box(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint8_t r, uint8_t g, uint8_t b)
+int grx_fillbox(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint8_t r, uint8_t g, uint8_t b)
 {
 		uint16_t i = 0;
+		uint16_t j = 0;
 
+	/*
 		for (i = y0 ; i < y1 ; i++) {
 				grx_line(x0, i, y1, i, r, g, b);
 				}
+	*/
+
+		for (j = y0 ; j < y1 ; j++) {
+			for (i = x0 ; i < x1 ; i++) {
+				grx_setpixel(i, j,  r, g, b);
+				}
+		}
+	
 
     return 0;
 }
